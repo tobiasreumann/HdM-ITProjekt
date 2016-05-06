882d0903d42367941b3fee553166c597faa2e1b7
Joel.Siffermann <joel.siffermann@gmail.com>
2016-05-06 13:45:11 +0200

    ReportGen und Interface struktur vorläufig

--- ITProjekt/src/de/hdm/itprojekt/server/report/ReportGeneratorImpl.java
+++ ITProjekt/src/de/hdm/itprojekt/server/report/ReportGeneratorImpl.java
@@ -1,5 +1,38 @@
 package de.hdm.itprojekt.server.report;
 
-public class ReportGeneratorImpl {
+import java.util.Vector;
 
+import de.hdm.itprojekt.server.db.AehnlichkeitsmassMapper;
+import de.hdm.itprojekt.server.db.PartnervorschlagMapper;
+import de.hdm.itprojekt.server.db.ProfilMapper;
+import de.hdm.itprojekt.server.db.SuchprofilMapper;
+import de.hdm.itprojekt.shared.ReportGeneratorInterface;
+import de.hdm.itprojekt.shared.bo.Profil;
+
+public class ReportGeneratorImpl implements ReportGeneratorInterface{
+
+	private static final long serialVersionUID = 1L;
+	
+	private ProfilMapper pMapper;
+	private AehnlichkeitsmassMapper aMapper;
+	private PartnervorschlagMapper pvMapper;
+	private SuchprofilMapper sMapper;
+	
+	public ReportGeneratorImpl() throws IllegalArgumentException{
+		
+	}
+	
+	public void init() throws IllegalArgumentException{
+		
+	}
+	
+	@Override
+	public Vector<Profil> erstelleNichtangesehenePartnervorschlaegeReport() throws IllegalArgumentException{
+		
+	}
+	
+	@Override
+	public Vector<Profil> erstellePartnervorschlaegeNachSuchprofilReport() throws IllegalArgumentException{
+		
+	}
 }
--- ITProjekt/src/de/hdm/itprojekt/shared/InterfaceReportGenerator.java
+++
@@ -1,5 +0,0 @@
-package de.hdm.itprojekt.shared;
-
-public interface InterfaceReportGenerator {
-
-}
