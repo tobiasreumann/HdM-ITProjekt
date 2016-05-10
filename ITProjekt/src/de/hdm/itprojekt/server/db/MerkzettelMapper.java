4d9e4193f8783cdb653769416b37746a9ffa3da4
Georg <Georg@Georg-Erich>
2016-05-10 09:44:19 +0200

    Insert und lösche methoden hinzugefügt

--- ITProjekt/src/de/hdm/itprojekt/server/db/MerkzettelMapper.java
+++ ITProjekt/src/de/hdm/itprojekt/server/db/MerkzettelMapper.java
@@ -1,5 +1,18 @@
 package de.hdm.itprojekt.server.db;
 
+import de.hdm.itprojekt.shared.bo.Merkzettel;
+import de.hdm.itprojekt.shared.bo.Profil;
+
 public class MerkzettelMapper {
+
+	public Merkzettel insert(Profil p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void vonMerkzettelLoeschen(Merkzettel m) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- ITProjekt/src/de/hdm/itprojekt/server/db/ProfilMapper.java
+++ ITProjekt/src/de/hdm/itprojekt/server/db/ProfilMapper.java
@@ -1,5 +1,13 @@
 package de.hdm.itprojekt.server.db;
 
+import de.hdm.itprojekt.shared.bo.Profil;
+import de.hdm.itprojekt.shared.bo.Suchprofil;
+
 public class ProfilMapper {
+
+	public Profil insert(Suchprofil sp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
